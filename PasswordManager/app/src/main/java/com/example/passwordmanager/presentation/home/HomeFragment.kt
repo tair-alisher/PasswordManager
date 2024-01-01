bf97b080d3f59a0f425cc3b884733cc930e63dae
tair-alisher <ur.alisher@mail.ru>
2024-01-01 15:27:12 +0600

    HomeScreen - search input field.

--- PasswordManager/app/src/main/java/com/example/passwordmanager/presentation/home/HomeFragment.kt
+++ PasswordManager/app/src/main/java/com/example/passwordmanager/presentation/home/HomeFragment.kt
@@ -1,11 +1,10 @@
 package com.example.passwordmanager.presentation.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.passwordmanager.R
 
 class HomeFragment : Fragment() {
@@ -21,12 +20,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
